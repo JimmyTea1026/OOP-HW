849e7981dc5e336cb346002d1d4fabb56a2a4aa3
Jimmy <jimmy87102@gmail.com>
2022-03-25 15:49:17 +0800

    case usecase rename 修改完成

---
+++ src/controller/classCanvasActionListener.java
@@ -0,0 +1,74 @@
+package controller;
+
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import utility.nodeType;
+import model.basicObject;
+import model.classObject;
+import model.node;
+
+public class classCanvasActionListener implements MouseInputListener{
+    private ArrayList <basicObject> objList;
+    private JPanel canvas;
+    int X, Y;
+
+    public classCanvasActionListener(JPanel c, ArrayList <basicObject> b){
+        canvas = c;
+        objList = b;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        X = e.getX();
+        Y = e.getY();
+        // TODO Auto-generated method stub
+        classObject newClassObj = new classObject(e.getX(), e.getY());
+        node newNode = new node(nodeType.type.ROOT);
+        newNode.setContent(newClassObj);
+        newClassObj.setNodePointer(newNode);
+        objList.add(newClassObj);
+        int dep = objList.indexOf(newClassObj);
+        newClassObj.setDepth(dep);
+        
+        canvas.repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
---
+++ src/controller/usecaseCanvasActionListener.java
@@ -0,0 +1,75 @@
+package controller;
+
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import javax.swing.*;
+import javax.swing.event.MouseInputListener;
+import utility.nodeType;
+import model.basicObject;
+import model.classObject;
+import model.node;
+import model.usecaseObject;
+
+public class usecaseCanvasActionListener implements MouseInputListener{
+    private ArrayList <basicObject> objList;
+    private JPanel canvas;
+    int X, Y;
+
+    public usecaseCanvasActionListener(JPanel c, ArrayList <basicObject> b){
+        canvas = c;
+        objList = b;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        X = e.getX();
+        Y = e.getY();
+        // TODO Auto-generated method stub
+        usecaseObject newUsecaseObj = new usecaseObject(e.getX(), e.getY());
+        node newNode = new node(nodeType.type.ROOT);
+        newNode.setContent(newUsecaseObj);
+        newUsecaseObj.setNodePointer(newNode);
+        objList.add(newUsecaseObj);
+        int dep = objList.indexOf(newUsecaseObj);
+        newUsecaseObj.setDepth(dep);
+
+        canvas.repaint();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
